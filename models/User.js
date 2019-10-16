78fe753fb97e32b096cd1fbfcb3db09f9585af08
Alexis Huerta <alehuer90@gmail.com>
2019-10-16 16:31:14 -0700

    temp save

--- models/User.js
+++ models/User.js
@@ -1,24 +1,48 @@
 const Sequelize = require("sequelize");
+const Model = Sequelize.Model;
 const db = require("../database/db.js");
 
-var User = db.sequelize.define(
-  "tbl_user",
-  {
-    username: {
-      type: Sequelize.STRING,
-      primaryKey: true,
-    },
-    email: {
-      type: Sequelize.STRING
-    },
-    password: {
-        type: Sequelize.STRING
-    }
+//importing needed models
+const Definition = ("./Definition.js");
+
+class User extends Model {}
+User.init({
+  username: {
+    type: Sequelize.STRING,
+    primaryKey: true
   },
-  {
-    timestamps: false
+  email: {
+    type: Sequelize.STRING
+  },
+  password:{
+    type: Sequelize.STRING
   }
-);
+}, 
+{
+  sequelize,
+  modelName: 'user'
+});
+
+// var User = db.sequelize.define(
+//   "tbl_user",
+//   {
+//     username: {
+//       type: Sequelize.STRING,
+//       primaryKey: true,
+//     },
+//     email: {
+//       type: Sequelize.STRING
+//     },
+//     password: {
+//         type: Sequelize.STRING
+//     }
+//   },
+//   {
+//     timestamps: false
+//   }
+// );
+
+User.hasMany(Definition);
 
 User.sync({ alter: true });
 
