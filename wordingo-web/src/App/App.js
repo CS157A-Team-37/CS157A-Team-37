ed76fee714308731eca105cce7318ac127da75d5
Andrew <47757558+Dark-Everyday@users.noreply.github.com>
2019-10-29 12:36:02 -0700

    Added sidebar

--- wordingo-web/src/App/App.js
+++ wordingo-web/src/App/App.js
@@ -1,26 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import SideBar from './sidebar';
+import routes from "./routes";
+import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <SideBar />
+      <div id="page-wrap">
+        <h1>Wordingo</h1>
+      </div>
     </div>
   );
 }
 
-export default App;
