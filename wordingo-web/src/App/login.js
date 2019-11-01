a0e51217f211d3a0c269dba7698df52263f466cb
Andrew <47757558+Dark-Everyday@users.noreply.github.com>
2019-10-31 21:12:40 -0700

    added placeholder textfields

--- wordingo-web/src/App/login.js
+++ wordingo-web/src/App/login.js
@@ -4,8 +4,18 @@ import "./login.css";
 export default function login() {
   return (
     <div className="login">
-        <h1>Login</h1>
-        <p></p>
+       <h1>Login</h1>
+      <p></p>
+      <form>
+         <label>
+         Email: <input type="text" name="name" />
+         </label>
+         <label>
+         Password:
+         <input type="text" name="name" />
+         </label>
+         <input type="submit" value="Submit" />
+       </form>
     </div>
   );
 }
