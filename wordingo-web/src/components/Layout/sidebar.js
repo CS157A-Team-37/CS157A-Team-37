b7496902414270637a735211b765a84400150007
Alexis Huerta <alehuer90@gmail.com>
2019-11-24 17:52:14 -0800

    add navbar

--- wordingo-web/src/components/Layout/sidebar.js
+++ wordingo-web/src/components/Layout/sidebar.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 export default props => {
   return (
-    <Menu>
-      <a className="menu-item" href="/">
-      Home
-      </a>
-
-      <a className="menu-item" href="/login">
-        Login
-      </a>
-
-      <a className="menu-item" href="/category">
-        Browse Category
-      </a>
-
-      <a className="menu-item" href="/alphabetical">
-        Browse Alphabetical
-      </a>
-    </Menu>
-    
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/">Wordingo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/login">Login</Nav.Link>
+          <Nav.Link href="/alphabetical">Alphabetical</Nav.Link>
+          <NavDropdown title="Categories" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/category">Category1</NavDropdown.Item>
+            <NavDropdown.Item href="/category">Category1</NavDropdown.Item>
+            <NavDropdown.Item href="/category">Category1</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/test">Test</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
