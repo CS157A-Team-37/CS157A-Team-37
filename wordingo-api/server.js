c29e6b50c0707038590cd6e4d7ef143d853dfe51
Andrew <47757558+Dark-Everyday@users.noreply.github.com>
2019-12-03 06:28:52 -0800

    modified fetch functions

--- wordingo-api/server.js
+++ wordingo-api/server.js
@@ -1,17 +1,16 @@
+// Importing some dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 
-// importing routes
-var index = require("./routes/index");
+var models = require("./models");
+
+// Importing routes
+// var index = require("./routes/index");
 var words = require("./routes/words");
 var definitions = require("./routes/definitions");
 var users = require("./routes/users");
-var categories = require("./routes/categories")
-// var madeDefinitions = require("./routes/madeDefinitions")
-
-var cors = require("cors");
-
-var port = 5000;
+var categories = require("./routes/categories");
 
 var app = express();
 app.use(cors());
@@ -20,14 +19,14 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Routes
 //app.use("/", index);
 app.use("/api", words);
 app.use("/api", definitions);
 app.use("/api", users);
-app.use("/api", categories)
-// app.use("/api", madeDefinitions)
+app.use("/api", categories);
 
-
+var port = 5000;
 app.listen(port, function() {
   console.log("Wordingo API running on port 5000...");
 });
