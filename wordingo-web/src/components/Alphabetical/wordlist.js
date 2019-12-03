49a53c6d7bc937d77ab23b1418db871338c5f385
XiaohanSun <xiaohan.sun@sjsu.edu>
2019-12-02 23:24:21 -0800

    Delete all comments

--- wordingo-web/src/components/Alphabetical/wordlist.js
+++ wordingo-web/src/components/Alphabetical/wordlist.js
@@ -33,77 +33,9 @@ class Wordlist extends Component {
 
         return list;
 
-
-
-
-
-        // if(this.state.letterClicked=="All"){
-        //     const list = this.props.allwords.map(word => (
-        //         //the key has to be here on the outermost level
-        //         <div className="col-sm-4" key={word._id}>
-        //           <h1>{word.name}</h1>
-        //         </div>
-        //       ));
-        //     return list;
-        // }
-        // else if(this.state.letterClicked){
-            
-        //     console.log("GOT INTO ELSE IF");
-        //     console.log(this.state.letterClicked)
-
-        //     //   let list = this.props.allwords.filter(function (word) {
-        //     //     return word[0]===this.state.letterClicked;
-        //     //   });
-
-        //     let newList = this.props.allwords.filter(word => {
-        //         console.log(word.name[0] + "->" + this.state.letterClicked);
-        //         if(word[0] == this.state.letterClicked){
-        //             console.log("MATCHES");
-        //             return word[0] == this.state.letterClicked;
-        //         }
-        //         else{
-        //             console.log("NOT THE SAME");
-        //         }
-                
-        //     });
-        //     console.log(newList);
-        //     newList.map(word => (
-        //         //the key has to be here on the outermost level
-        //         <div className="col-sm-4" key={word._id}>
-        //             <h1>{word.name}</h1>
-        //         </div>
-        //     ));
-
-        //       return newList;
-
-        
-
-        // }
         
     }
-    // wordList = () => {
-    //     //map goes through every element in an array and does something(takes in callback)
-    //     const list = this.props.allwords.map(word => (
-    //       //the key has to be here on the outermost level
-    //       <div className="col-sm-4" key={word._id}>
-    //         <h1>{word.name}</h1>
-    //       </div>
-    //     ));
-
-    //     return list;
-    // }
-    // letterList = () => {
-    //     //map goes through every element in an array and does something(takes in callback)
-    //     const filteredList = //filtered list (allwords.filter(...based on..))
-    //     filteredList.map(word => (
-    //       //the key has to be here on the outermost level
-    //       <div className="col-sm-4" key={word._id}>
-    //         <h1>{word.name}</h1>
-    //       </div>
-    //     ));
-
-    //     return list;
-    // }
+    
 
 
 
