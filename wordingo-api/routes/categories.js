df82937ed7c7d8ef891cc3d5e39f690d875337ff
Andrew <47757558+Dark-Everyday@users.noreply.github.com>
2019-12-07 13:33:56 -0800

    Made categories function select only distinct

--- wordingo-api/routes/categories.js
+++ wordingo-api/routes/categories.js
@@ -1,13 +1,32 @@
 var express = require("express");
 var router = express.Router();
+var Sequelize = require("sequelize");
 
 const cors = require("cors");
 const Category = require("../models").Category;
 
+
 router.use(cors());
 
 router.get("/categories", function(req, res, next) {
-  Category.findAll()
+  Category.findAll({
+    attributes: [
+      [Sequelize.fn('DISTINCT', Sequelize.col('name')), 'name'],
+    ],
+  })
+    .then(categories => {
+      console.log("herein categories");
+      res.json(categories);
+    })
+    .catch(err => {
+      res.send("error: " + err);
+    });
+});
+
+router.get("/categories/:categoryName", function(req, res, next) {
+  Category.findAll( {where: {
+    name: req.params.categoryName
+  }})
     .then(categories => {
       res.json(categories);
     })
